cd42813fca51661382e051cbc38e6e1ef0471c40
BrAt4iTeRa <71733287+BrAt4iTeRa@users.noreply.github.com>
2020-09-23 12:55:27 +0300

    staraya laba_002 variant 28

---
+++ Matrica.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // русификация консоли
+    int n, i, j;
+    long  int mat[100][100], s = 0;
+    printf_s("Введите размер матрицы (натурального значения)");
+    scanf_s("%d", &n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf_s("%ld", &mat[i][j]);
+        }
+    }
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (mat[i][j] % 2 == 1) s = s + mat[i][j];
+            else if (mat[i][j] % 2 == -1) s = s + mat[i][j];
+        }
+    }
+    printf_s("Ваша сумма нечетных элементов = %ld", s);
+    return 0;
+}
